1dc1e9de78dc57e695787ff94f2d3c7ad0fcf25a
ValDesign <valdesign.dev@gmail.com>
2024-08-18 19:57:06 +0200

    🏗 Started manual update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onBeforeMount } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import { check } from '@tauri-apps/plugin-updater';
 import { useRouter } from "vue-router";
 import { Config } from "@/utils/types";
 
@@ -10,6 +11,13 @@ onBeforeMount(async () => {
   console.log("Checking if the app is registered");
   const config = await invoke<Config | null>("get_config");
   if (config === null) router.push({ path: "/register", replace: true });
+
+  const update = await check();
+  if (update) {
+    console.log(`Found update ${update.version} from ${update.date} with notes ${update.body}`);
+  } else {
+    console.log("No update available");
+  }
 });
 </script>
 
